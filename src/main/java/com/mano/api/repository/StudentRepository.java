c14026e9f7d698a162652b6e015568a5427fd8c8
Manoel C <manoelclemente9@outlook.com>
2023-02-17 22:39:09 -0300

    business logic whatever

--- src/main/java/com/mano/api/repository/StudentRepository.java
+++ src/main/java/com/mano/api/repository/StudentRepository.java
@@ -3,11 +3,15 @@ package com.mano.api.repository;
 
 import com.mano.api.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
-
+//    @Query("SELECT s FROM Strudent s where s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 
 }
